3fd808122dd57d95725beae3f5be0ba1df7d4ade
woukl22 <woukl22@gmail.com>
2024-10-12 04:09:49 +0900

    Feat: layout

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      aspectRatio: {
+        '3/2': '3 / 2',
+      },
+    },
+  },
+  plugins: [],
+}
+
